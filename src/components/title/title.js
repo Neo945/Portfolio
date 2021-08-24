38a3353e8980f3e1516cb582e56df2eaf67da060
Neo945 <shreeshsrvstv@gmail.com>
2021-08-25 02:35:45 +0530

    Add: content in home page

--- src/components/title/title.js
+++ src/components/title/title.js
@@ -1,5 +1,34 @@
 import "./title.css";
 
+function Blink(params) {
+  return (
+    <span
+      id="text"
+      style={{ height: "10px", margin: "10px", marginLeft: "0px" }}
+    ></span>
+  );
+}
+
 export default function Title(params) {
-  return <div>Title</div>;
+  return (
+    <div id={"title"}>
+      <div>
+        <div className={"text"}>
+          {"Finally, you found me!"
+            .split("")
+            .map((ele) => (ele === " " ? <pre>{ele}</pre> : <div>{ele}</div>))}
+        </div>
+        <div className={"text"}>
+          {"Welcome to my Protfolio"
+            .split("")
+            .map((ele) => (ele === " " ? <pre>{ele}</pre> : <div>{ele}</div>))}
+        </div>
+        <div className={"text"}>
+          {"Where do you wanna go?"
+            .split("")
+            .map((ele) => (ele === " " ? <pre>{ele}</pre> : <div>{ele}</div>))}
+        </div>
+      </div>
+    </div>
+  );
 }
