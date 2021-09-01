32bc2a1db8eb7d3c815218ff093ce53b00abb57d
Neo945 <shreeshsrvstv@gmail.com>
2021-09-01 20:17:16 +0530

    Add: card animation

--- src/components/projects/project.js
+++ src/components/projects/project.js
@@ -4,9 +4,15 @@ function Card(props) {
   return (
     <div
       id={"card"}
-      style={{ transform: `translateX(-${25 * props.overlap}%)` }}
+      style={{ transform: `translateX(-${35 * props.overlap}%)` }}
+      onMouseEnter={(event) => (event.target.style.width = "250px")}
+      onMouseLeave={(event) => (event.target.style.width = "200px")}
     >
-      <div></div>
+      <div class="shadow"></div>
+      <div className={"card-content"}>
+        <div className={"card-title"}>{"props.title"}</div>
+        <div className={"card-body"}>{"props.body"}</div>
+      </div>
     </div>
   );
 }
@@ -18,10 +24,11 @@ export default function Project(props) {
         <div className={"project-title"}>Projects</div>
         <div
           className={"project-content"}
-          style={{ transform: "translate(15%)" }}
+          style={{ transform: "translate(20%)" }}
         >
-          {[1, 2, 3, 4, 5, 6, 7].map((ele) => (
-            <Card overlap={ele} />
+          {[1].map((ele) => (
+            // {[1, 2, 3, 4, 5, 6].map((ele) => (
+            <Card overlap={ele + 1} />
           ))}
         </div>
       </div>
