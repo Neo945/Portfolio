6ce73cade8e231c57fc5ce56e757c0583b284319
Neo945 <shreeshsrvstv@gmail.com>
2021-09-02 20:43:54 +0530

    Error: Animation project

--- src/components/projects/project.js
+++ src/components/projects/project.js
@@ -1,17 +1,33 @@
 import "./project.css";
+import list from "./data.json";
 
 function Card(props) {
   return (
     <div
-      id={"card"}
-      style={{ transform: `translateX(-${35 * props.overlap}%)` }}
-      onMouseEnter={(event) => (event.target.style.width = "250px")}
-      onMouseLeave={(event) => (event.target.style.width = "200px")}
+      className={"card"}
+      style={{ left: `${props.content.id * 10}%` }}
+      onMouseOver={async () => {
+        for (let i = props.content.id - 2; i >= 0; i--) {
+          document.getElementsByClassName("card")[
+            i
+          ].style.transform = `translateX(-${20 * (i + 1)}px)`;
+        }
+        for (let i = list.length - props.content.id; i > 0; i--) {
+          document.getElementsByClassName("card")[
+            6 - i
+          ].style.transform = `translateX(${350 - 70 * (6 - i)}px)`;
+        }
+      }}
+      onMouseLeave={async () => {
+        for (let i = 0; i < 6; i++) {
+          document.getElementsByClassName("card")[i].style.transform = `none`;
+        }
+      }}
     >
-      <div class="shadow"></div>
+      <div className="shadow"></div>
       <div className={"card-content"}>
-        <div className={"card-title"}>{"props.title"}</div>
-        <div className={"card-body"}>{"props.body"}</div>
+        <div className={"card-title"}>{props.content.title}</div>
+        <div className={"card-body"}>{"props.content.description"}</div>
       </div>
     </div>
   );
@@ -20,18 +36,14 @@ function Card(props) {
 export default function Project(props) {
   return (
     <div id={"proj"}>
-      <div>
-        <div className={"project-title"}>Projects</div>
-        <div
-          className={"project-content"}
-          style={{ transform: "translate(20%)" }}
-        >
-          {[1].map((ele) => (
-            // {[1, 2, 3, 4, 5, 6].map((ele) => (
-            <Card overlap={ele + 1} />
+      <>
+        {/* <div className={"project-title"}>Projects</div> */}
+        <div className={"project-content"}>
+          {list.map((ele) => (
+            <Card overlap={ele.id + 1} content={ele} key={`${ele.id}`} />
           ))}
         </div>
-      </div>
+      </>
     </div>
   );
 }
