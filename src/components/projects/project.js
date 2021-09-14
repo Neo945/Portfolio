6aaeb4a17f6dc7030ab55ecc4a8c5ce21c74cfa4
Neo945 <shreeshsrvstv@gmail.com>
2021-09-14 21:55:50 +0530

    Modify: projects page

--- src/components/projects/project.js
+++ src/components/projects/project.js
@@ -1,33 +1,29 @@
 import "./project.css";
 import list from "./data.json";
-import { useState } from "react";
 
 function Card(props) {
   return (
-    <div
-      className={"card"}
-      style={{ left: `${props.content.id * 10}%` }}
-      onMouseOver={async () => {
-        const list = document.getElementsByClassName("card");
-        const id = props.content.id;
-        for (let i = id; i < list.length; i++) {
-          list[i].style.transform = `translateX(${
-            50 * (6 - i + id)
-          }px) rotateY(0deg)`;
-        }
-      }}
-      onMouseLeave={async () => {
-        for (let i = 0; i < 6; i++) {
-          document.getElementsByClassName("card")[
-            i
-          ].style.transform = `rotateY(20deg)`;
-        }
-      }}
-    >
-      <div className="shadow"></div>
-      <div className={"card-content"}>
-        <div className={"card-title"}>{props.content.title}</div>
-        {/* <div className={"card-body"}>{props.content.description}</div> */}
+    <div className={"card"}>
+      <div>
+        <div className={"card-content"}>
+          <div className={"card-title"}>{props.content?.title}</div>
+          <div
+            className={"card-body"}
+            style={{ fontSize: "1.5em", width: "100%", height: "auto" }}
+          >
+            {props.content?.description}
+          </div>
+          <a
+            href={props.content?.git}
+            className={"github-link"}
+            style={{ textDecoration: "none", color: "black" }}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div>Project link</div>
+            <div className={"next"}></div>
+          </a>
+        </div>
       </div>
     </div>
   );
@@ -37,7 +33,18 @@ export default function Project(props) {
   return (
     <div id={"proj"}>
       <>
-        <div className={"project-title"}>Projects</div>
+        <div
+          className={"project-title"}
+          style={{
+            width: "80%",
+            fontWeight: "bold",
+            fontSize: "4em",
+            margin: "2.5%",
+            marginTop: "5%",
+          }}
+        >
+          Projects
+        </div>
         <div className={"project-content"}>
           {list.map((ele) => (
             <Card overlap={ele.id + 1} content={ele} key={`${ele.id}`} />
